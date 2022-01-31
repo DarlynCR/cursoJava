158c644882c7d039201f452639dabaa9aa4be70b
DarlynCaicedo <darlyn.caicedo.calderon@gmail.com>
2022-01-31 15:35:10 -0500

    Varargs

--- src/enums/Main.java
+++ src/enums/Main.java
@@ -24,6 +24,20 @@ public class Main {
 
 
 
+
+        /*VARARGS
+        * Argumentos variables -> cuando no sabemos cuantos argumentos se van a recibir en un método
+        * El argumento variable debe ser el último argumento en el método -> si va a recibir más parámetros
+        * Una vez recibido el vararg en el método, dentro del mismo, es tratado como un arreglo
+        * sintaxis -> tipoDato... nombreVariable
+         */
+        //Al llamar al método se pueden pasar una cantidad de arg variables
+        showNumbers(1,5,3,4);
+        showNumbers(2,5);
+
+        //Método que recibe un vararg y otro parámetro
+        showVararg("Sergio", 1, 2, 3);
+        showVararg("Darlyn", 1, 23);
     }
 
 
@@ -37,5 +51,21 @@ public class Main {
             case MONDAY:
                 System.out.println("Segundo día de la semana");
         }
-    };
+    }
+
+    //Ejemplo de vararg
+    public static void showNumbers(int... numbers){
+        //Una vez recibido el argumento, es tratado como un arreglo
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Elemento: " + numbers[i]);
+        }
+    }
+
+    //El parámetro vararg debe ser el último al recibir en el método
+    public static void showVararg(String name, int... numbers){
+        System.out.println("Nombre: " +  name);
+        //Se imprimen los numeros con el método showNumbers
+        showNumbers(numbers);
+    }
+
 }
